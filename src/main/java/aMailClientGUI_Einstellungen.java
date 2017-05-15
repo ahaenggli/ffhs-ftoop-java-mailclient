bbdf7a5a7d8a8f63e8f114e9edfc66b102342be5
ahaenggli <adriano@haenggli.net>
2017-05-15 14:16:54 +0200

    Einstellungen ausgelagert

---
+++ src/main/java/aMailClientGUI_Einstellungen.java
@@ -0,0 +1,152 @@
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+public class aMailClientGUI_Einstellungen extends JDialog {
+
+	
+
+	private static final long serialVersionUID = -8439476419298094881L;
+
+	public aMailClientGUI_Einstellungen() {
+		super();
+		//super("adriano's MailClient - Einstellungen");
+		// Set the default behaviour for the close button
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Erzeugung eines neuen Dialoges
+		//JDialog meinJDialog = new JDialog();
+		setTitle("Einstellungen");
+		setSize(450, 300);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+		JPanel panelGreen = new JPanel();
+
+		panelGreen.setBackground(Color.GREEN);
+
+		// Erzeugung eines JTabbedPane-Objektes
+		JTabbedPane tabpane = new JTabbedPane(SwingConstants.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
+
+		// POP3
+		// Hier erzeugen wir unsere JPanels
+		JPanel panelRot = new JPanel();
+		// Hier setzen wir die Hintergrundfarben für die JPanels
+		panelRot.setBackground(Color.RED);
+
+		// Raster erstellen
+		JLabel pop3_server_label = new JLabel("POP3-Server: ");
+		JLabel pop3_port_label = new JLabel("POP3-Server-Port: ");
+		JLabel pop3_username_label = new JLabel("Username: ");
+		JLabel pop3_password_label = new JLabel("Password: ");
+
+		JTextField pop3_serverField = new JTextField(aMailClientSettings.getPop3Server(), 20);
+		JTextField pop3_usernameField = new JTextField(aMailClientSettings.getPop3User(), 20);
+		JPasswordField pop3_passwordField = new JPasswordField(aMailClientSettings.getPop3PW(), 20);
+		JTextField pop3_portField = new JTextField(aMailClientSettings.getPop3Port(), 20);
+
+		pop3_server_label.setDisplayedMnemonic(KeyEvent.VK_S);
+		pop3_server_label.setLabelFor(pop3_serverField);
+		pop3_port_label.setDisplayedMnemonic(KeyEvent.VK_O);
+		pop3_port_label.setLabelFor(pop3_portField);
+		pop3_username_label.setDisplayedMnemonic(KeyEvent.VK_U);
+		pop3_username_label.setLabelFor(pop3_usernameField);
+		pop3_password_label.setDisplayedMnemonic(KeyEvent.VK_P);
+		pop3_password_label.setLabelFor(pop3_passwordField);
+
+		panelRot.add(pop3_server_label);
+		panelRot.add(pop3_serverField);
+		panelRot.add(pop3_port_label);
+		panelRot.add(pop3_portField);
+		panelRot.add(pop3_username_label);
+		panelRot.add(pop3_usernameField);
+		panelRot.add(pop3_password_label);
+		panelRot.add(pop3_passwordField);
+
+		JButton pop3_save = new JButton("Speichern");
+		pop3_save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				aMailClientSettings.setPop3Server(pop3_serverField.getText());
+				aMailClientSettings.setPop3User(pop3_usernameField.getText());
+				aMailClientSettings.setPop3PW(new String(pop3_passwordField.getPassword()));
+				aMailClientSettings.setPop3Port(pop3_portField.getText());
+			}
+		});
+
+		panelRot.add(pop3_save);
+
+		// Hier werden die JPanels als Registerkarten hinzugefügt
+		tabpane.addTab("POP3", panelRot);
+
+		// smtp
+
+		JPanel panelBlue = new JPanel();
+		panelBlue.setBackground(Color.CYAN);
+
+		// Raster erstellen
+		JLabel smtp_server_label = new JLabel("smtp-Server: ");
+		JLabel smtp_port_label = new JLabel("smtp-Server-Port: ");
+		JLabel smtp_username_label = new JLabel("Username: ");
+		JLabel smtp_password_label = new JLabel("Password: ");
+
+		JTextField smtp_serverField = new JTextField(aMailClientSettings.getsmtpServer(), 20);
+		JTextField smtp_usernameField = new JTextField(aMailClientSettings.getsmtpUser(), 20);
+		JPasswordField smtp_passwordField = new JPasswordField(aMailClientSettings.getsmtpPW(), 20);
+		JTextField smtp_portField = new JTextField(aMailClientSettings.getsmtpPort(), 20);
+
+		smtp_server_label.setDisplayedMnemonic(KeyEvent.VK_S);
+		smtp_server_label.setLabelFor(smtp_serverField);
+		smtp_port_label.setDisplayedMnemonic(KeyEvent.VK_O);
+		smtp_port_label.setLabelFor(smtp_portField);
+		smtp_username_label.setDisplayedMnemonic(KeyEvent.VK_U);
+		smtp_username_label.setLabelFor(smtp_usernameField);
+		smtp_password_label.setDisplayedMnemonic(KeyEvent.VK_P);
+		smtp_password_label.setLabelFor(smtp_passwordField);
+
+		panelBlue.add(smtp_server_label);
+		panelBlue.add(smtp_serverField);
+		panelBlue.add(smtp_port_label);
+		panelBlue.add(smtp_portField);
+		panelBlue.add(smtp_username_label);
+		panelBlue.add(smtp_usernameField);
+		panelBlue.add(smtp_password_label);
+		panelBlue.add(smtp_passwordField);
+
+		JButton smtp_save = new JButton("Speichern");
+		smtp_save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				aMailClientSettings.setsmtpServer(smtp_serverField.getText());
+				aMailClientSettings.setsmtpUser(smtp_usernameField.getText());
+				aMailClientSettings.setsmtpPW(new String(smtp_passwordField.getPassword()));
+				aMailClientSettings.setsmtpPort(smtp_portField.getText());
+			}
+		});
+
+		panelBlue.add(smtp_save);
+
+		// Hier werden die JPanels als Registerkarten hinzugefügt
+		tabpane.addTab("SMTP", panelBlue);
+
+		tabpane.addTab("Diverses", panelGreen);
+
+		// JTabbedPane wird unserem Dialog hinzugefügt
+		add(tabpane);
+		// Wir lassen unseren Dialog anzeigen
+		setModal(true);
+		setVisible(true);
+		
+	}
+	
+}
--- src/main/java/aMailClientGUI__Main.java
+++ src/main/java/aMailClientGUI__Main.java
@@ -42,7 +42,7 @@ import javax.swing.tree.DefaultTreeModel;
 
 import javax.swing.*;
 
-public class aMailClientGUI extends JFrame {
+public class aMailClientGUI__Main extends JFrame {
 
 	private static final long serialVersionUID = 8508453892315236372L;
 	private JLabel StatusBar = new JLabel("StatusBar");
@@ -88,126 +88,7 @@ public class aMailClientGUI extends JFrame {
 	}
 
 	public void ShowSettingDialog() {
-		// Erzeugung eines neuen Dialoges
-		JDialog meinJDialog = new JDialog();
-		meinJDialog.setTitle("Einstellungen");
-		meinJDialog.setSize(450, 300);
-		meinJDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-		JPanel panelGreen = new JPanel();
-
-		panelGreen.setBackground(Color.GREEN);
-
-		// Erzeugung eines JTabbedPane-Objektes
-		JTabbedPane tabpane = new JTabbedPane(SwingConstants.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
-
-		// POP3
-		// Hier erzeugen wir unsere JPanels
-		JPanel panelRot = new JPanel();
-		// Hier setzen wir die Hintergrundfarben für die JPanels
-		panelRot.setBackground(Color.RED);
-
-		// Raster erstellen
-		JLabel pop3_server_label = new JLabel("POP3-Server: ");
-		JLabel pop3_port_label = new JLabel("POP3-Server-Port: ");
-		JLabel pop3_username_label = new JLabel("Username: ");
-		JLabel pop3_password_label = new JLabel("Password: ");
-
-		JTextField pop3_serverField = new JTextField(aMailClientSettings.getPop3Server(), 20);
-		JTextField pop3_usernameField = new JTextField(aMailClientSettings.getPop3User(), 20);
-		JPasswordField pop3_passwordField = new JPasswordField(aMailClientSettings.getPop3PW(), 20);
-		JTextField pop3_portField = new JTextField(aMailClientSettings.getPop3Port(), 20);
-
-		pop3_server_label.setDisplayedMnemonic(KeyEvent.VK_S);
-		pop3_server_label.setLabelFor(pop3_serverField);
-		pop3_port_label.setDisplayedMnemonic(KeyEvent.VK_O);
-		pop3_port_label.setLabelFor(pop3_portField);
-		pop3_username_label.setDisplayedMnemonic(KeyEvent.VK_U);
-		pop3_username_label.setLabelFor(pop3_usernameField);
-		pop3_password_label.setDisplayedMnemonic(KeyEvent.VK_P);
-		pop3_password_label.setLabelFor(pop3_passwordField);
-
-		panelRot.add(pop3_server_label);
-		panelRot.add(pop3_serverField);
-		panelRot.add(pop3_port_label);
-		panelRot.add(pop3_portField);
-		panelRot.add(pop3_username_label);
-		panelRot.add(pop3_usernameField);
-		panelRot.add(pop3_password_label);
-		panelRot.add(pop3_passwordField);
-
-		JButton pop3_save = new JButton("Speichern");
-		pop3_save.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ev) {
-				aMailClientSettings.setPop3Server(pop3_serverField.getText());
-				aMailClientSettings.setPop3User(pop3_usernameField.getText());
-				aMailClientSettings.setPop3PW(new String(pop3_passwordField.getPassword()));
-				aMailClientSettings.setPop3Port(pop3_portField.getText());
-			}
-		});
-
-		panelRot.add(pop3_save);
-
-		// Hier werden die JPanels als Registerkarten hinzugefügt
-		tabpane.addTab("POP3", panelRot);
-
-		// smtp
-
-		JPanel panelBlue = new JPanel();
-		panelBlue.setBackground(Color.CYAN);
-
-		// Raster erstellen
-		JLabel smtp_server_label = new JLabel("smtp-Server: ");
-		JLabel smtp_port_label = new JLabel("smtp-Server-Port: ");
-		JLabel smtp_username_label = new JLabel("Username: ");
-		JLabel smtp_password_label = new JLabel("Password: ");
-
-		JTextField smtp_serverField = new JTextField(aMailClientSettings.getsmtpServer(), 20);
-		JTextField smtp_usernameField = new JTextField(aMailClientSettings.getsmtpUser(), 20);
-		JPasswordField smtp_passwordField = new JPasswordField(aMailClientSettings.getsmtpPW(), 20);
-		JTextField smtp_portField = new JTextField(aMailClientSettings.getsmtpPort(), 20);
-
-		smtp_server_label.setDisplayedMnemonic(KeyEvent.VK_S);
-		smtp_server_label.setLabelFor(smtp_serverField);
-		smtp_port_label.setDisplayedMnemonic(KeyEvent.VK_O);
-		smtp_port_label.setLabelFor(smtp_portField);
-		smtp_username_label.setDisplayedMnemonic(KeyEvent.VK_U);
-		smtp_username_label.setLabelFor(smtp_usernameField);
-		smtp_password_label.setDisplayedMnemonic(KeyEvent.VK_P);
-		smtp_password_label.setLabelFor(smtp_passwordField);
-
-		panelBlue.add(smtp_server_label);
-		panelBlue.add(smtp_serverField);
-		panelBlue.add(smtp_port_label);
-		panelBlue.add(smtp_portField);
-		panelBlue.add(smtp_username_label);
-		panelBlue.add(smtp_usernameField);
-		panelBlue.add(smtp_password_label);
-		panelBlue.add(smtp_passwordField);
-
-		JButton smtp_save = new JButton("Speichern");
-		smtp_save.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ev) {
-				aMailClientSettings.setsmtpServer(smtp_serverField.getText());
-				aMailClientSettings.setsmtpUser(smtp_usernameField.getText());
-				aMailClientSettings.setsmtpPW(new String(smtp_passwordField.getPassword()));
-				aMailClientSettings.setsmtpPort(smtp_portField.getText());
-			}
-		});
-
-		panelBlue.add(smtp_save);
-
-		// Hier werden die JPanels als Registerkarten hinzugefügt
-		tabpane.addTab("SMTP", panelBlue);
-
-		tabpane.addTab("Diverses", panelGreen);
-
-		// JTabbedPane wird unserem Dialog hinzugefügt
-		meinJDialog.add(tabpane);
-		// Wir lassen unseren Dialog anzeigen
-		meinJDialog.setModal(true);
-		meinJDialog.setVisible(true);
-
+		new aMailClientGUI_Einstellungen();
 	}
 
 	public JMenuBar guiGetMenu() {
@@ -355,13 +236,9 @@ public class aMailClientGUI extends JFrame {
 		table_mailListe.addMouseListener(new java.awt.event.MouseAdapter() {
 			@Override
 			public void mouseClicked(java.awt.event.MouseEvent evt) {
-				
 		
 				if (evt.getClickCount() == 2) {
-					String texti;
-					texti = getSelectedMailListRow(evt.getPoint(), 3);
-					JOptionPane.showMessageDialog(null, texti, "Nachricht", JOptionPane.OK_CANCEL_OPTION);
-
+					new aMailClientGUI_MailFenster(getSelectedMailListRow(evt.getPoint(), 2), getSelectedMailListRow(evt.getPoint(), 3));
 				}
 			}
 		});
@@ -377,7 +254,7 @@ public class aMailClientGUI extends JFrame {
 		return scroll;
 	}
 
-	public aMailClientGUI() {
+	public aMailClientGUI__Main() {
 		super("adriano's MailClient - GUI");
 		// Set the default behaviour for the close button
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -418,7 +295,7 @@ public class aMailClientGUI extends JFrame {
 	 *            Not used
 	 */
 	public static void main(String args[]) {
-		new aMailClientGUI();
+		new aMailClientGUI__Main();
 
 	}
 
