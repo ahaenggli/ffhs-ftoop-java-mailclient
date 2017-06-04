92a5fc9d32e353c18a33097cbdc8f8e860b2c33c
ahaenggli <adriano@haenggli.net>
2017-06-04 22:34:11 +0200

    JavaDoc ergaenzt; Formatiert

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -2,10 +2,10 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 /**
- * Klasse mit allen wichtigen Einstellungen zum MailClient Da überall immer
- * dieselben Werte gebruacht werden (fix) sind sämtliche public Methoden
- * "static" Für die Datenspeicherung wird der Default-Typ "Preferences" aus Java
- * verwendet. So ist gewährleistet, dass der Client Plattformübergreifend läuft.
+ * Klasse mit allen wichtigen Einstellungen zum MailClient Da ueberall immer
+ * dieselben Werte gebruacht werden (fix) sind saemtliche public Methoden
+ * "static" Fuer die Datenspeicherung wird der Default-Typ "Preferences" aus Java
+ * verwendet. So ist gewaehrleistet, dass der Client Plattformuebergreifend laeuft.
  * 
  * @author ahaen
  *
@@ -22,19 +22,19 @@ public final class Configuration {
 
 	// Name des MailClients
 	private static final String MailClientName = "Bananaa MailClient";
-	
+
 	// Hauptzweig (Root) vom Gesamten MailClient-Datenspeicher.
 	private static Preferences prefs = Preferences.userRoot().node("/ch/ahaenggli/MailClient");
 
 	// Hier liegen die Mailordner und Mails drin
 	private static final String NameRootFolder = "Folders";
-	private static Preferences folders = getPrefs().node(NameRootFolder);	
+	private static Preferences folders = getPrefs().node(NameRootFolder);
 	private static final String NamePosteingang = "Neue Mails";
 	private static final String NamePostausgang = "Gesendet";
-	
-	// Debug Params für Sys-Outputs
+
+	// Debug Params fuer Sys-Outputs
 	private static boolean Debug = true;
-	
+
 	// pop3-Einstellungen (Mail senden)
 	private final static String POP3Server = "POP3Server";
 	private final static String POP3User = "POP3User";
@@ -49,30 +49,40 @@ public final class Configuration {
 
 	// Diverses-Einstellungen
 	private final static String anzahlMinuten = "autoLadenMinuten";
-	
-	  
+
 	/*
-	 * Private Methoden für internen Klassen-internen gebrauch
+	 * Private Methoden fuer internen Klassen-internen gebrauch
 	 */
 
 	/**
-	 * Erstellt einen neuen Ordner für Einstellungen (oder Mails), falls nocht
-	 * nicht existent
+	 * Erstellt einen neuen Ordner fuer Einstellungen (oder Mails), falls noch
+	 * nicht existent.
 	 * 
 	 * @param Name
+	 *            Name des neuen Ordners
 	 * @param SortNr
+	 *            LaufNr des Ordners fuer Sortierung
 	 * @param parent
-	 * @throws BackingStoreException
+	 *            Parent-Ordner
+	 * @return Erfolg (=Ordner gibt es (nun)) true|false
 	 */
-	public static void createFolder(String Name, int SortNr, Preferences parent) throws BackingStoreException {
-		if (!parent.nodeExists(Name)) {
-			parent.node(Name).putInt("SortNr", SortNr);
+
+	public static boolean createFolder(String Name, int SortNr, Preferences parent) {
+		boolean returnWert = false;
+		try {
+			if (!parent.nodeExists(Name)) {
+				parent.node(Name).putInt("SortNr", SortNr);
+			}
+			returnWert = true;
+		} catch (BackingStoreException e) {
+			returnWert = false;
 		}
+		return returnWert;
 	}
 
 	/**
-	 * Gibt den gesuchten Wert zurück, falls vorhanden. Existiert der Wert
-	 * nicht, kommt leer zurück.
+	 * Gibt den gesuchten Wert zurueck, falls vorhanden. Existiert der Wert
+	 * nicht, kommt leer zurueck.
 	 * 
 	 * @param Name
 	 *            Name des gesuchten Wertes
@@ -82,21 +92,19 @@ public final class Configuration {
 		try {
 			prefs.sync();
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 		return prefs.get(Name, "");
 	}
 
 	/**
-	 * Setzt einen Wert zu einem Namen
+	 * Setzt einen Wert zu einem Namen (nur fuer internen Gebrauch)
 	 * 
 	 * @param Name
 	 *            Name des zu speichernen Wertes
 	 * @param Value
 	 *            Wert zum Speichern
-	 * @return true|false: Gibt true bei Erfolg zurück, sonst false
+	 * @return true|false: Gibt true bei Erfolg zurueck, sonst false
 	 */
 	private final static boolean setValue(String Name, String Value) {
 		boolean result = false;
@@ -107,57 +115,54 @@ public final class Configuration {
 			prefs.sync();
 			result = true;
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 			result = false;
 		}
 		return result;
 	}
 
 	/*
-	 * Public (static) Methoden für den Zugriff auf die Daten Alle anderen
-	 * Classen und Methoden greifen nur auf diese zu Wenn hier etwas geändert
-	 * wird, gilt es somit fürs ganze MailClient-Projekt
+	 * Public (static) Methoden fuer den Zugriff auf die Daten Alle anderen
+	 * Classen und Methoden greifen nur auf diese zu Wenn hier etwas geaendert
+	 * wird, gilt es somit fuers ganze MailClient-Projekt
 	 * 
 	 */
 
 	/**
-	 * Löscht alle Daten
+	 * Loescht alle Daten
 	 */
-	public final static void deleteConfig()
-	{
+	public final static void deleteConfig() {
 		try {
 			prefs.removeNode();
 			prefs.flush();
-			 prefs = Preferences.userRoot().node("/ch/ahaenggli/MailClient");
+			prefs = Preferences.userRoot().node("/ch/ahaenggli/MailClient");
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
-		
-		
+
 	}
-	
-	
-	/** 
-	 * Gibt den MailClientName zurück
+
+	/**
+	 * Gibt den MailClientName zurueck
+	 * 
 	 * @return MailClientName
 	 */
-	public final static String getMailClientName()
-	{
+	public final static String getMailClientName() {
 		return MailClientName;
 	}
-	
+
 	/**
-	 * Gibt zurück ob Debug Aufruf oder nicht
+	 * Gibt zurueck ob Debug Aufruf oder nicht (Div. System.out.* werden dadurch
+	 * aktiviert/deaktiviert)
+	 * 
 	 * @return true|false
 	 */
-	public final static boolean getDebug(){
+	public final static boolean getDebug() {
 		return Debug;
 	}
-	
+
 	/**
-	 * Gibt das Root-Verzeichnis der Einstellungen zurück
+	 * Gibt das Root-Verzeichnis der Einstellungen zurueck
 	 * 
 	 * @return Preferences mit Einstellungs-Root
 	 */
@@ -166,58 +171,57 @@ public final class Configuration {
 	}
 
 	/**
-	 * Gibt das Mail-Ordner-Verzeichnis zurück (hier liegen die Mails drin)
+	 * Gibt das Mail-Ordner-Verzeichnis zurueck (hier liegen die Mails drin)
 	 * 
 	 * @return Preferences mit Mailordner
 	 */
 	public final static Preferences getFolders() {
-		
-		
+
 		// Sicherstellen, dass Ein- und Ausgang existieren
 		try {
 			getEingang();
 			getGesendet();
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
-		
-		
-		
+
 		return folders;
 	}
 
-	
 	/**
-	 * Gibt den Namen des Postausgangs zurück
+	 * Gibt den Namen des Postausgangs zurueck
+	 * 
 	 * @return Name von Postausgang
 	 */
-	public final static String getNamePostausgang(){
-		return  NamePostausgang;
+	public final static String getNamePostausgang() {
+		return NamePostausgang;
 	}
-	
+
 	/**
-	 * Gibt den Namen des Posteingang zurück
+	 * Gibt den Namen des Posteingangs zurueck
+	 * 
 	 * @return Name von Posteingang
 	 */
-	public final static String getNamePosteingang(){
+	public final static String getNamePosteingang() {
 		return NamePosteingang;
 	}
-	
+
 	/**
-	 * Gibt den Namen des NameRootFolder zurück
+	 * Gibt den Namen des NameRootFolders zurueck (in dem alle anderen Ordner
+	 * wie Posteingang/ausgang liegen)
+	 * 
 	 * @return Name von NameRootFolder
 	 */
-	public final static String getNameRootFolder(){
+	public final static String getNameRootFolder() {
 		return NameRootFolder;
 	}
-	
+
 	/*
-	 * POP 3 Einstellungen lesen/schreiben für andere Klassen/Methoden
+	 * POP 3 Einstellungen lesen/schreiben fuer andere Klassen/Methoden
 	 */
 
 	/**
-	 * Gibt POP3 Server-Name zurück
+	 * Gibt POP3 Server-Name zurueck
 	 * 
 	 * @return String mit POP3 Server Name
 	 */
@@ -230,8 +234,8 @@ public final class Configuration {
 	 * Setzt den POP3 Server-Name
 	 * 
 	 * @param newValue
-	 *            Neuer Wert für den POP3 Server Name
-	 * @return Gibt bei Erfolg true zurück, sonst false
+	 *            Neuer Wert fuer POP3Server
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setPop3Server(String newValue) {
 		return setValue(POP3Server, newValue);
@@ -246,7 +250,8 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer den POP3User
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setPop3User(String newValue) {
 		return setValue(POP3User, newValue);
@@ -261,7 +266,8 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer POP3PW
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setPop3PW(String newValue) {
 		return setValue(POP3PW, newValue);
@@ -276,14 +282,15 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer POP3Port
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setPop3Port(String newValue) {
 		return setValue(POP3Port, newValue);
 	}
 
 	/*
-	 * STMP Einstellungen lesen/schreiben für andere Klassen/Methoden
+	 * STMP Einstellungen lesen/schreiben fuer andere Klassen/Methoden
 	 */
 	/**
 	 * @return
@@ -294,7 +301,8 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer smtpServer
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setsmtpServer(String newValue) {
 		return setValue(smtpServer, newValue);
@@ -309,7 +317,8 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer smtpUser
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setsmtpUser(String newValue) {
 		return setValue(smtpUser, newValue);
@@ -324,7 +333,8 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fue smtpPW
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 
 	public final static boolean setsmtpPW(String newValue) {
@@ -340,89 +350,113 @@ public final class Configuration {
 
 	/**
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert fuer smtpPort
+	 * @return Gibt bei Erfolg true zurueck, sonst false
 	 */
 	public final static boolean setsmtpPort(String newValue) {
 		return setValue(smtpPort, newValue);
 	}
 
-	
-	public static Preferences getOrdner(String x, Preferences parent) throws BackingStoreException{
-		
-		if(parent == null) parent = folders;
-		
+	/**
+	 * Ermittelt spezifischen Ordner und erstellt ihn falls noetig
+	 * 
+	 * @param x
+	 *            Ordner zum suchen/erstellen
+	 * @param parent
+	 *            Wo soll Ordner gesucht/erstellt werden
+	 * @return Gefundener Ordner
+	 * @throws BackingStoreException
+	 */
+	public static Preferences getOrdner(String x, Preferences parent) throws BackingStoreException {
+
+		if (parent == null)
+			parent = folders;
+
 		if (!parent.nodeExists(x))
 			createFolder(x, 1, parent);
 
-		
 		return parent.node(x);
 	}
-	
+
 	/**
-	 * Gibt Standard-Verzeichnis für Neue Mails
+	 * Gibt Standard-Verzeichnis fuer Neue Mails
 	 * 
 	 * @return Preferences
-	 * @throws BackingStoreException 
+	 * @throws BackingStoreException
 	 */
 	public static Preferences getEingang() {
-		
+
 		try {
 			if (!folders.nodeExists(NamePosteingang))
 				createFolder(NamePosteingang, 1, folders);
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
 
-		
 		return folders.node(NamePosteingang);
 	}
 
 	/**
-	 * Gibt sStandard-Verzeichnis für gesendete Mails
+	 * Gibt sStandard-Verzeichnis fuer gesendete Mails
 	 * 
 	 * @return Preferences
-	 * @throws BackingStoreException 
+	 * @throws BackingStoreException
 	 */
 	public static Preferences getGesendet() throws BackingStoreException {
-		
+
 		if (!folders.nodeExists(NamePostausgang))
 			createFolder(NamePostausgang, 2, folders);
 		return folders.node(NamePostausgang);
 	}
 
+	/**
+	 * Gibt Anzahl Minuten fuer AutoRunner zurueck
+	 * 
+	 * @return Zahl
+	 */
 	public final static Long getAnzahlminuten() {
 		Long min = (long) 0;
-		
-		try{
+
+		try {
 			min = Long.parseLong(getValue(anzahlMinuten));
-		}catch(Exception e){
+		} catch (Exception e) {
 			min = (long) 0;
 		}
 		return min;
 	}
 
 	/**
+	 * Setze Wert fuer anzahlMinuten (AutoRunner)
+	 * 
 	 * @param newValue
-	 * @return
+	 *            Neuer Wert
+	 * @return Gibt bei Erfolg true zuruech, sonst false
 	 */
 	public final static boolean setAnzahlminuten(String newValue) {
 		return setValue(anzahlMinuten, newValue);
 	}
 
-	
+	/**
+	 * Ordner kopieren (fuer Umbenennung in Hauptfenster)
+	 * 
+	 * @param src
+	 *            Von wo soll kopiert werden
+	 * @param newNode
+	 *            Zu war soll kopiert werden
+	 * @throws BackingStoreException
+	 *             Fehler
+	 */
 	public final static void copyNode(Preferences src, Preferences newNode) throws BackingStoreException {
-        if (src.childrenNames().length > 0) {
-            for (String child : src.childrenNames()) {
-                copyNode(src.node(child), newNode.node(child));
-            }
-        }
-        String[] keys = src.keys();
-        for (String key : keys) {
-            String value = src.get(key, "");
-            newNode.put(key, value);
-        }
-    }
- 
-	
+		if (src.childrenNames().length > 0) {
+			for (String child : src.childrenNames()) {
+				copyNode(src.node(child), newNode.node(child));
+			}
+		}
+		String[] keys = src.keys();
+		for (String key : keys) {
+			String value = src.get(key, "");
+			newNode.put(key, value);
+		}
+	}
+
 }
